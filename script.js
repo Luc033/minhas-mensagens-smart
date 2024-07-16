27723fa3fd7ad5adf003a2f45f0bdc0f2853ebb2
Luc033 <lucasrm33.contato@gmail.com>
2024-07-16 19:56:19 -0300

    mensagem de cópia e mudança no layout

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+const msgCopiado = document.getElementById('msgCopiado');
 document.querySelectorAll(".title").forEach((item) => {
   item.addEventListener("click", (event) => {
     const phrase = event.target.getAttribute("data-phrase");
     navigator.clipboard
       .writeText(phrase)
       .then(() => {
-        alert("Frase copiada: " + phrase);
+          mostraImagem(msgCopiado);
       })
       .catch((err) => {
         console.error("Erro ao copiar frase: ", err);
       });
   });
 });
+
+function mostraImagem(element){
+  element.style.display = 'flex';
+  setTimeout(function() {
+    if(element.style.display === 'flex')
+    element.style.display = 'none';
+   
+}, 3000);
+}
